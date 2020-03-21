860c61bf07c8c6a0be89b521c69a2c34a6fbdb93
sujithkumar104 <sujithkumar104@gmail.com>
2020-03-21 22:29:31 +0530

    New Features

--- src/com/concepts/annomous/Test.java
+++ src/com/concepts/annomous/Test.java
@@ -4,7 +4,7 @@ public class Test {
 	
 	
 	public static void main(String[] args) {
-		
+							
 		Thread t = new Thread(
 			() -> {
 				for (int i=0;i<10;i++) {
--- src/com/concepts/newfeatures/MethodReference/Test3.java
+++ src/com/concepts/newfeatures/MethodReference/Test3.java
@@ -1,9 +1,7 @@
 package com.concepts.newfeatures.MethodReference;
 
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class Test3 {
